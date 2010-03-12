1cb7d76a0fb671e22767ce03ebf61a52630534b5
israelcr741 <israelcr741@b9c2913c-2618-11df-a76c-adfa62dee608>
2010-03-12 00:34:40 +0000

    Subo la interfaz medio hecha y un poco del controlador para probarlo.

--- G20P1/src/logica/Principal.java
+++ G20P1/src/logica/Principal.java
@@ -1,13 +1,23 @@
 package logica;
 
+import gui.Ventana;
+
 public class Principal {
 
+	private Ventana v = null;
+	
+	public Principal(Ventana v) {
+		this.v = v;
+	}
+	
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Ventana v = new Ventana();
+		Principal p = new Principal(v);
 	}
 
 }
